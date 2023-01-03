cab5e98459c29cd0d2fa771d6b805e5f491062ce
BatoBalint <balint.bato@gmail.com>
2023-01-03 17:56:58 +0100

    first param check done with test output

--- main.c
+++ main.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
-    
-    printf("Arg. count: %d\n", argc - 1);
-    for (int i = 1; i < argc; i++) {
-        printf("%s\n", argv[i]);
+    int lineNumbering = 0;
+    if (argc != 3) {
+        printf("\nUsage:\n\trev [SHOW LINE NUMBERS] [MAX LINE LENGTH] files...\n\n");
+        return 1;
     }
+
+    if (strcmp(argv[1], "linenums") == 0) lineNumbering = 1;
+    else if (strcmp(argv[1], "nolinenums") == 0);
+    else {
+        printf("First parameter should be either \"linenums\" or \"nolinenums\"\n");
+        return 1;
+    }
+
+    if (lineNumbering) printf("Line numbering is on.\n");
+    else printf("Line numbering is off.\n");
 
     return 0;
 }
