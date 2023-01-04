3d9c0436f8c515dca908faec139751dfd4a5a046
BatoBalint <balint.bato@gmail.com>
2023-01-04 13:36:44 +0100

    quicksave

--- main.c
+++ main.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 #include <string.h>
 
+int validateArgs();
+
 int main(int argc, char *argv[]) {
-    int lineNumbering = 0;
-    if (argc != 3) {
+    int lineNumbering;
+    int maxLineLength;
+
+    if (argc < 3) {
         printf("\nUsage:\n\trev [SHOW LINE NUMBERS] [MAX LINE LENGTH] files...\n\n");
         return 1;
     }
 
     if (strcmp(argv[1], "linenums") == 0) lineNumbering = 1;
-    else if (strcmp(argv[1], "nolinenums") == 0);
+    else if (strcmp(argv[1], "nolinenums") == 0) lineNumbering = 0;
     else {
         printf("First parameter should be either \"linenums\" or \"nolinenums\"\n");
         return 1;
@@ -18,5 +22,9 @@ int main(int argc, char *argv[]) {
     if (lineNumbering) printf("Line numbering is on.\n");
     else printf("Line numbering is off.\n");
 
+    return 0;
+}
+
+int validateArgs() {
     return 0;
 }
